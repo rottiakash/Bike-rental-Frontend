650b2539ff953391da5f3fd52c4f0c41771edcd8
rottiakash <rottiakash@gmail.com>
2020-12-07 16:07:55 +0530

    Added Spinners

--- Components/Payment/Payment.tsx
+++ Components/Payment/Payment.tsx
@@ -5,6 +5,7 @@ import "react-credit-cards/es/styles-compiled.css";
 import { DatePicker } from "antd";
 import { Form, Input, Button, Checkbox } from "antd";
 import { useRouter } from "next/router";
+import Spinner from "../../HOCs/spinner";
 interface PaymentProps {
   config: config;
 }
@@ -18,6 +19,7 @@ interface Card {
 }
 
 const Payment: FC<PaymentProps> = ({ config }) => {
+  const [spinning, setSpinning] = useState(false);
   const router = useRouter();
   const [card, setCard] = useState<Card>({
     cvc: "",
@@ -27,99 +29,102 @@ const Payment: FC<PaymentProps> = ({ config }) => {
     number: "",
   });
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
-      }}
-    >
-      <Cards
-        cvc={card.cvc}
-        expiry={card.expiry}
-        focused={card.focus}
-        name={card.name}
-        number={card.number}
-      />
-      <Form
-        style={{ marginTop: "40px" }}
-        onFinish={() => {
-          console.log(config.reserve_payload);
-          //POST TO SERVER
-          router.push("/success");
+    <Spinner spinning={spinning}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
         }}
       >
-        <Form.Item
-          label="Card Number"
-          name="Card Number"
-          rules={[
-            { required: true, message: "Please input your Card Number!" },
-          ]}
+        <Cards
+          cvc={card.cvc}
+          expiry={card.expiry}
+          focused={card.focus}
+          name={card.name}
+          number={card.number}
+        />
+        <Form
+          style={{ marginTop: "40px" }}
+          onFinish={() => {
+            setSpinning(true);
+            console.log(config.reserve_payload);
+            //POST TO SERVER
+            router.push("/success");
+          }}
         >
-          <Input
-            type="tel"
-            name="number"
-            placeholder="Card Number"
-            onChange={(e) => setCard({ ...card, number: e.target.value })}
-            onFocus={(e) => setCard({ ...card, focus: e.target.name })}
-          />
-        </Form.Item>
-        <Form.Item
-          label="Card Holder's Name"
-          name="Card Holder's Name"
-          rules={[
-            { required: true, message: "Please input Card Holder's Name!" },
-          ]}
-        >
-          <Input
-            type="text"
-            name="name"
-            placeholder="Card Holder Name"
-            onChange={(e) => setCard({ ...card, name: e.target.value })}
-            onFocus={(e) => setCard({ ...card, focus: e.target.name })}
-          />
-        </Form.Item>
-        <Form.Item label="Valid Through" name="Valid Through">
-          <DatePicker
-            onChange={(date, _) =>
-              setCard({ ...card, expiry: date.format("MM") })
-            }
-            format={"MM"}
-            picker="month"
-          />
-          <DatePicker
-            picker="year"
-            onChange={(date, _) =>
-              setCard({
-                ...card,
-                expiry: (card.expiry += "/" + date.format("YY")),
-              })
-            }
-            format={"YY"}
-          />
-        </Form.Item>
-        <Form.Item
-          label="CVV"
-          name="CVV"
-          rules={[{ required: true, message: "Please input CVV" }]}
-        >
-          <Input
-            type="tel"
-            name="cvc"
-            placeholder="CVV/CVC"
-            onChange={(e) => setCard({ ...card, cvc: e.target.value })}
-            onFocus={(e) => setCard({ ...card, focus: e.target.name })}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form.Item
+            label="Card Number"
+            name="Card Number"
+            rules={[
+              { required: true, message: "Please input your Card Number!" },
+            ]}
+          >
+            <Input
+              type="tel"
+              name="number"
+              placeholder="Card Number"
+              onChange={(e) => setCard({ ...card, number: e.target.value })}
+              onFocus={(e) => setCard({ ...card, focus: e.target.name })}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Card Holder's Name"
+            name="Card Holder's Name"
+            rules={[
+              { required: true, message: "Please input Card Holder's Name!" },
+            ]}
+          >
+            <Input
+              type="text"
+              name="name"
+              placeholder="Card Holder Name"
+              onChange={(e) => setCard({ ...card, name: e.target.value })}
+              onFocus={(e) => setCard({ ...card, focus: e.target.name })}
+            />
+          </Form.Item>
+          <Form.Item label="Valid Through" name="Valid Through">
+            <DatePicker
+              onChange={(date, _) =>
+                setCard({ ...card, expiry: date.format("MM") })
+              }
+              format={"MM"}
+              picker="month"
+            />
+            <DatePicker
+              picker="year"
+              onChange={(date, _) =>
+                setCard({
+                  ...card,
+                  expiry: (card.expiry += "/" + date.format("YY")),
+                })
+              }
+              format={"YY"}
+            />
+          </Form.Item>
+          <Form.Item
+            label="CVV"
+            name="CVV"
+            rules={[{ required: true, message: "Please input CVV" }]}
+          >
+            <Input
+              type="tel"
+              name="cvc"
+              placeholder="CVV/CVC"
+              onChange={(e) => setCard({ ...card, cvc: e.target.value })}
+              onFocus={(e) => setCard({ ...card, focus: e.target.name })}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Spinner>
   );
 };
 
--- Components/Selection/selection.tsx
+++ Components/Selection/selection.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Bike, config } from "../../pages/book";
 import { Button, DatePicker, Space } from "antd";
 import { Form } from "antd";
 import { Select } from "antd";
 import * as a from "axios";
+import Spinner from "../../HOCs/spinner";
 const axios = a.default;
 const { Option } = Select;
 const { RangePicker } = DatePicker;
@@ -13,61 +14,65 @@ interface SelectionProps {
 }
 
 const Selection: FC<SelectionProps> = ({ config }) => {
+  const [spinning, setSpinning] = useState(false);
   return (
-    <Form
-      onFinish={async (values) => {
-        const payload = {
-          startDate: values.dates[0].format(dateFormat),
-          endDate: values.dates[1].format(dateFormat),
-          location: values.location,
-        };
-        //POST PAYLOAD - GET BIKES
-        const bikes: Array<Bike> = [
-          {
-            id: 1,
-            name: "Bullet",
-            price: 100,
-            img_url:
-              "https://imgd.aeplcdn.com/393x221/bw/models/royal-enfield-bullet-350-ks--x--efi-bs-vi20200401130113.jpg?q=85",
-          },
-        ];
-        config.setBikes(bikes);
-        config.setStartDate(payload.startDate);
-        config.setEndDate(payload.endDate);
-        config.setStage(1);
-      }}
-    >
-      <Form.Item
-        label="Start and End date for the Rental"
-        name="dates"
-        rules={[{ required: true, message: "Please input the Date Range" }]}
+    <Spinner spinning={spinning}>
+      <Form
+        onFinish={async (values) => {
+          setSpinning(true);
+          const payload = {
+            startDate: values.dates[0].format(dateFormat),
+            endDate: values.dates[1].format(dateFormat),
+            location: values.location,
+          };
+          //POST PAYLOAD - GET BIKES
+          const bikes: Array<Bike> = [
+            {
+              id: 1,
+              name: "Bullet",
+              price: 100,
+              img_url:
+                "https://imgd.aeplcdn.com/393x221/bw/models/royal-enfield-bullet-350-ks--x--efi-bs-vi20200401130113.jpg?q=85",
+            },
+          ];
+          config.setBikes(bikes);
+          config.setStartDate(payload.startDate);
+          config.setEndDate(payload.endDate);
+          config.setStage(1);
+        }}
       >
-        <RangePicker format={dateFormat} />
-      </Form.Item>
-      <Form.Item
-        label="Pickup/Drop Location"
-        name="location"
-        rules={[
-          {
-            required: true,
-            message: "Please input the Location for Pick-up/Drop",
-          },
-        ]}
-      >
-        <Select showSearch placeholder={"Select the location"}>
-          {config.locations.map((x) => (
-            <Option value={x} key={x}>
-              {x}
-            </Option>
-          ))}
-        </Select>
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Start and End date for the Rental"
+          name="dates"
+          rules={[{ required: true, message: "Please input the Date Range" }]}
+        >
+          <RangePicker format={dateFormat} />
+        </Form.Item>
+        <Form.Item
+          label="Pickup/Drop Location"
+          name="location"
+          rules={[
+            {
+              required: true,
+              message: "Please input the Location for Pick-up/Drop",
+            },
+          ]}
+        >
+          <Select showSearch placeholder={"Select the location"}>
+            {config.locations.map((x) => (
+              <Option value={x} key={x}>
+                {x}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Spinner>
   );
 };
 
