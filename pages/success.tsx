85fe69f6ba0553d335fe0ade9311920479939919
Akash Rotti <rottiakash@gmail.com>
2021-01-03 13:52:47 +0530

    Added Review

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,7 +1,25 @@
 import Container from "../HOCs/container";
 import { Result, Button } from "antd";
+import { Typography, notification } from "antd";
+import { Divider } from "antd";
+import { Rate } from "antd";
+
+const desc = ["Terrible", "Bad", "Normal", "Good", "Wonderful"];
+const { Title } = Typography;
+
+const openNotification = () => {
+  notification.open({
+    message: "Review Has been Submitted",
+    description: "Thank you for your valuable review",
+    placement: "bottomRight",
+  });
+};
+
 import Link from "next/link";
+import { useState } from "react";
 export default function Success() {
+  const [rating, setRating] = useState<number>();
+  const [disabled, setDisabled] = useState(false);
   return (
     <Container>
       <div
@@ -10,6 +28,7 @@ export default function Success() {
           justifyContent: "center",
           alignItems: "center",
           height: "100%",
+          flexDirection: "column",
         }}
       >
         <Result
@@ -23,6 +42,29 @@ export default function Success() {
             </Link>,
           ]}
         />
+        <Divider />
+        <Title>Rate the experience</Title>
+        <Rate
+          tooltips={disabled ? null : desc}
+          onChange={(value) => setRating(value)}
+          value={rating}
+          disabled={disabled}
+        />
+        {rating ? (
+          <span className="ant-rate-text">{desc[rating - 1]}</span>
+        ) : (
+          "Not Rated"
+        )}
+        <Button
+          type="primary"
+          onClick={() => {
+            openNotification();
+            setDisabled(true);
+          }}
+          disabled={disabled}
+        >
+          Submit Rating
+        </Button>
       </div>
       ,
     </Container>
